dfe903fbf4234f3632d676795249159d37e58a71
yuna0318 <seogeae4@gmail.com>
2026-05-13 13:10:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ b=st.selectbox('무엇을 좋아하시나요?',['오선','콜라','산책'])
 if st.button('인사말 생성'):
   st.write(a+'님 안녕하세요!')
   st.info('반갑습니다')
-  st.warning(b+'좋은 답안이네요! 저도 좋아해요.)
-  st.error('잘 지내봅시다 a+님!')
+  st.warning(b+'좋은 답안이네요! 저도 좋아해요')
+  st.error('잘 지내봅시다!')
+  st.balloons()
