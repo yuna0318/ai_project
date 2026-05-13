82f04737ff69852340e09858831bf5816dfa9f99
yuna0318 <seogeae4@gmail.com>
2026-05-13 13:10:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ a=st.text_input('이름이 무엇인가요?')
 b=st.selectbox('무엇을 좋아하시나요?',['오선','콜라','산책'])
 if st.button('인사말 생성'):
   st.write(a+'님 안녕하세요!')
+  st.info('반갑습니다')
+  st.warning(b+'좋은 답안이네요! 저도 좋아해요.)
+  st.error('잘 지내봅시다 a+님!')
